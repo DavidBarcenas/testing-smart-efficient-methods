77868564a7f2ea20d11e2c616eae4a1c32e99eb2
David Barcenas <daveepro@outlook.com>
2021-12-31 14:20:53 -0600

    add test function to assertion library

--- fundamentals/assertion-library.js
+++ fundamentals/assertion-library.js
@@ -1,14 +1,26 @@
 const { sum, subtract } = require('./math')
 
-let result, expected
+test('sum add numbers', () => {
+  const result = sum(10, 5)
+  const expected = 15
+  expect(result).tobe(expected)
+})
 
-result = sum(10, 5)
-expected = 15
-expect(result).tobe(expected)
+test('subtract subtracts numbers', () => {
+  const result = subtract(10, 5)
+  const expected = 5
+  expect(result).tobe(expected)
+})
 
-result = subtract(10, 5)
-expected = 5
-expect(result).tobe(expected)
+function test(title, callback) {
+  try {
+    callback()
+    console.log(`✓ ${title}`)
+  } catch (error) {
+    console.error(`✗ ${title}`)
+    console.error(error)
+  }
+}
 
 function expect(actual) {
   return {
