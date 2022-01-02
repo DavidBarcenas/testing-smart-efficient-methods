9a55bf29229c8ebb898dc40a3869f24037416eda
David Barcenas <daveepro@outlook.com>
2022-01-01 19:59:54 -0600

    override object props to mock

--- mocking/gameWar.js
+++ mocking/gameWar.js
@@ -2,20 +2,20 @@ const utils = require('./utils')
 
 function gameWar(player1, player2) {
   const numberToWin = 2
-  const player1Wins = 0
-  const player2Wins = 0
+  let player1Wins = 0
+  let player2Wins = 0
 
   while (player1Wins < numberToWin && player2Wins < numberToWin) {
     const winner = utils.getWinner(player1, player2)
 
     if (winner === player1) {
-      player1Wins += 1
+      player1Wins++
     } else if (winner === player2) {
-      player2Wins += 1
+      player2Wins++
     }
   }
 
-  return player1wins > player2Wins ? player1 : player2
+  return player1Wins > player2Wins ? player1 : player2
 }
 
 module.exports = gameWar
