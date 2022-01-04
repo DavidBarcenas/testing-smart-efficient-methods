194fc13931db9acace23960cf62f519e5d882ab6
David Barcenas <daveepro@outlook.com>
2022-01-03 19:35:56 -0600

    start static analysis

---
+++ static-analysis/src/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const username = 'David'
+typeof username === 'string'
+
+if (!'serviceWorker' in navigator) {
+  // you have an old browser
+}
+
+const greeting = 'hello'
+console.log('${greeting} world')
+[1, 2, 3].forEach(x => console.log(x))
