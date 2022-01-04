2344db71b9f3c34aa1fc34116178a1765e7e8e56
David Barcenas <daveepro@outlook.com>
2022-01-04 11:50:51 -0600

    install and configure typescript

---
+++ static-analysis/src/ts-example.ts
@@ -0,0 +1,20 @@
+function add (a: number, b:number) {
+  return a + b;
+}
+
+interface User {
+  name: {
+    first: string;
+    middle: string;
+    last: string;
+  }
+}
+
+
+function getFullName (user: User): string {
+  const {first, middle, last} = user.name
+  return [first, middle, last].filter(Boolean).join('')
+}
+
+add(1,2)
+getFullName({name: {first: 'Roger', middle: 'Smith', last: 'Bass'}})
