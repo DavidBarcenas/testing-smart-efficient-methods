2802bce91ab4b48434be90eced73cca952c6bbec
David Barcenas <daveepro@outlook.com>
2022-01-12 15:28:30 -0600

    create mock for console error with spyOn

--- react-components/src/__tests__/error-boundary.js
+++ react-components/src/__tests__/error-boundary.js
@@ -4,7 +4,9 @@ import { reportError as mockReportError } from '../http-request/api'
 
 jest.mock('../http-request/api')
 
+beforeAll(() => jest.spyOn(console, 'error').mockImplementation(() => { }))
 afterEach(() => jest.clearAllMocks())
+afterAll(() => console.error.mockRestore())
 
 function MyCustomError({ shouldThrow }) {
   if (shouldThrow) {
@@ -31,6 +33,8 @@ test('calls reportError and renders that there was a problem', () => {
 
   const error = expect.any(Error)
   const info = { componentStack: expect.stringContaining('MyCustomError') }
+
   expect(mockReportError).toHaveBeenCalledWith(error, info)
   expect(mockReportError).toHaveBeenCalledTimes(1)
+  expect(console.error).toHaveBeenCalledTimes(2)
 })
