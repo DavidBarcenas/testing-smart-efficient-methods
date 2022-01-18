81b707c944fb3ca5579d859c3d608fce81855759
David Barcenas <daveepro@outlook.com>
2022-01-17 22:37:39 -0600

    chore

--- react-components/src/__tests__/alert.js
+++ react-components/src/__tests__/alert.js
@@ -1,5 +1,5 @@
 /* eslint-disable testing-library/no-node-access */
-import { render, screen, waitFor } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import user from '@testing-library/user-event'
 import Alert from '../components/alert'
 
