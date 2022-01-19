d779f5f03c7a9d5b30bd27668f67996bb76c3e59
David Barcenas <daveepro@outlook.com>
2022-01-18 21:13:00 -0600

    redux: create a custom render function

--- react-components/src/__tests__/redux/connection.js
+++ react-components/src/__tests__/redux/connection.js
@@ -1,28 +1,30 @@
-import { render, screen } from '@testing-library/react'
+import { render as rtlRender, screen } from '@testing-library/react'
 import user from '@testing-library/user-event'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import Counter from '../../redux/counter'
-import { store } from '../../redux/store'
 import { reducer } from '../../redux/reducer'
 
+function render(ui, { initialState, store = createStore(reducer, initialState), ...rtlOptions } = {}) {
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>
+  }
+  return {
+    ...rtlRender(ui, { wrapper: Wrapper, ...rtlOptions }),
+    store
+  }
+}
+
 test('can render with redux with defaults', () => {
-  render(
-    <Provider store={store}>
-      <Counter />
-    </Provider>
-  )
+  render(<Counter />)
   user.click(screen.getByText('+'))
   expect(screen.getByLabelText(/count/i)).toHaveTextContent('1')
 })
 
 test('can render with redux with custom initial state', () => {
-  const store = createStore(reducer, { count: 3 })
-  render(
-    <Provider store={store}>
-      <Counter />
-    </Provider>
-  )
+  render(<Counter />, {
+    initialState: { count: 3 }
+  })
   user.click(screen.getByText('-'))
   expect(screen.getByLabelText(/count/i)).toHaveTextContent('2')
 })
