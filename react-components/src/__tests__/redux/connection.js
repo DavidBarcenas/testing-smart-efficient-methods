3fc1964951fb769b09be3c0eba7aa69e9ac13705
David Barcenas <daveepro@outlook.com>
2022-01-18 21:02:24 -0600

    redux: render with initial state

--- react-components/src/__tests__/redux/connection.js
+++ react-components/src/__tests__/redux/connection.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react'
 import user from '@testing-library/user-event'
+import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import Counter from '../../redux/counter'
 import { store } from '../../redux/store'
+import { reducer } from '../../redux/reducer'
 
 test('can render with redux with defaults', () => {
   render(
@@ -12,4 +14,15 @@ test('can render with redux with defaults', () => {
   )
   user.click(screen.getByText('+'))
   expect(screen.getByLabelText(/count/i)).toHaveTextContent('1')
+})
+
+test('can render with redux with custom initial state', () => {
+  const store = createStore(reducer, { count: 3 })
+  render(
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  )
+  user.click(screen.getByText('-'))
+  expect(screen.getByLabelText(/count/i)).toHaveTextContent('2')
 })
